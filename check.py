ae8f66780542647d79332d00e1d537812d2c886f
jiayu9923 <jiayu9923@gmail.com>
2023-05-26 15:25:32 +1000

    auto segment

--- check.py
+++ check.py
@@ -1,11 +1,75 @@
 import onnxruntime
+import torch
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
 
-ort_session = onnxruntime.InferenceSession('sam_onnx.onnx')
+sys.path.append("..")
 
-# Get input names
-input_names = [input.name for input in ort_session.get_inputs()]
-print('Input names: ', input_names)
+# load image
+image = cv2.imread('img/street_down.png')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# Get input shapes
-input_shapes = [input.shape for input in ort_session.get_inputs()]
-print('Input shapes: ', input_shapes)
+
+# display image
+# plt.figure(figsize=(10,10))
+# plt.imshow(image)
+# plt.axis('on')
+# plt.show()
+
+# some set-up
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:, :, 3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+
+    ax.imshow(img)
+    # return img
+
+
+checkpoint = "model/sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+
+device = "cuda"
+
+print("Loading -")
+sam = sam_model_registry[model_type](checkpoint=checkpoint)
+sam.to(device=device)
+
+print("Loading --")
+mask_generator = SamAutomaticMaskGenerator(sam)
+
+print("Loading ---")
+masks = mask_generator.generate(image)
+
+
+
+print("Loading ----")
+plt.figure(figsize=(20,20))
+plt.imshow(image)
+show_anns(masks)
+plt.axis('off')
+plt.savefig('foo.png')
+# plt.show()
+
+
+
+
+# # Get input names
+# input_names = [input.name for input in ort_session.get_inputs()]
+# print('Input names: ', input_names)
+#
+# # Get input shapes
+# input_shapes = [input.shape for input in ort_session.get_inputs()]
+# print('Input shapes: ', input_shapes)
--- server.py
+++ server.py
@@ -4,38 +4,97 @@ import onnxruntime as rt
 import numpy as np
 import io
 import base64
+import cv2
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
+import matplotlib.pyplot as plt
+from io import BytesIO
+from flask_cors import CORS
+
 
 
 
 app = Flask(__name__)
+CORS(app)
 
-sess = rt.InferenceSession("sam_onnx.onnx")
+sess = rt.InferenceSession("model/sam_onnx.onnx")
 
 @app.route('/segment', methods=['POST'])
 def segment():
     if 'file' not in request.files:
         return 'No file part', 400
-    file = request.files['file']
-    image = Image.open(io.BytesIO(file.read())).convert('RGB')
-    orig_size = image.size
-    image = np.array(image).astype('float32') / 255
-    image = np.transpose(image, (2, 0, 1))
-    image = np.expand_dims(image, axis=0)
+    file = request.files['file']\
 
-    input_name = sess.get_inputs()[0].name
-    output_name = sess.get_outputs()[0].name
-    result = sess.run([output_name], {input_name: image})[0]
+    # Convert FileStorage to PIL Image
+    pil_image = Image.open(file.stream).convert('RGB')
 
-    result = result[0] # remove batch dimension
-    result = np.transpose(result, (1, 2, 0)) # move channel dimension to the end
-    result = (result * 255).astype('uint8') # scale values back to [0, 255]
+    # Convert PIL Image to OpenCV array (BGR)
+    image = np.array(pil_image)
+    # Convert RGB to BGR
+    image = image[:, :, ::-1].copy()
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    output_image = Image.fromarray(result).resize(orig_size) # resize to original size
-    output_image_byte_arr = io.BytesIO()
-    output_image.save(output_image_byte_arr, format='PNG') # convert PIL Image to byte array
-    encoded_image = base64.b64encode(output_image_byte_arr.getvalue()).decode('ascii') # encode as base64
+    # display image
+    # plt.figure(figsize=(10,10))
+    # plt.imshow(image)
+    # plt.axis('off')
+    # plt.savefig('test.png')
 
-    return jsonify({'image': encoded_image})
+
+    def show_anns(anns):
+        if len(anns) == 0:
+            return
+        sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+        ax = plt.gca()
+        ax.set_autoscale_on(False)
+
+        img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+        img[:, :, 3] = 0
+        for ann in sorted_anns:
+            m = ann['segmentation']
+            color_mask = np.concatenate([np.random.random(3), [0.35]])
+            img[m] = color_mask
+        ax.imshow(img)
+        # return img
+
+    checkpoint = "model/sam_vit_h_4b8939.pth"
+    model_type = "vit_h"
+
+    device = "cuda"
+
+    print("Loading 1")
+    sam = sam_model_registry[model_type](checkpoint=checkpoint)
+    sam.to(device=device)
+
+    print("Loading 2")
+    mask_generator = SamAutomaticMaskGenerator(sam)
+
+    print("Loading 3")
+    masks = mask_generator.generate(image)
+
+    print("Loading 4")
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    show_anns(masks)
+    plt.axis('off')
+    plt.savefig('img/res.png')
+    # plt.show()
+
+    print("Loading 5")
+    res = cv2.imread('img/res.png')
+    is_success, buffer = cv2.imencode(".png", res)
+    if not is_success:
+        return jsonify({'error': 'Error during image conversion'}), 500
+
+    print("Loading 6")
+    # Convert buffer to byte stream
+    byte_stream = BytesIO(buffer)
+
+    print("Loading 7")
+    # Convert byte stream to Base64
+    base64_encoded_image = base64.b64encode(byte_stream.getvalue()).decode('utf-8')
+
+    # Return as JSON
+    return jsonify({'image': base64_encoded_image})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8181)
